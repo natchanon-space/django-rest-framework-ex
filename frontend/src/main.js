bd1025ec081d9904e4be631d19d4bd7d5809c00b
natchanon-space <natchanon.space@gmail.com>
2021-10-11 21:06:55 +0700

    add v-calendar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import VCalendar from 'v-calendar'
 
 axios.defaults.baseURL = "http://127.0.0.1:8000"
 
-createApp(App).use(store).use(router, axios).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router, axios)
+    .use(VCalendar, {})
+    .mount('#app')
